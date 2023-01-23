3b69a393624037550a4db333219eef6f17ad57cf
MRGuziX <tomasz.guzik@onet.eu>
2023-01-23 15:10:42 +0100

    Feature Name[Ticket_Number]: - Functions now are returning lists that contains five book titles and authors. - Class BookTitleWizardCreator now returning(printing) list within which author and title is jointed together with `zip` method.

--- book_title_wizzard.py
+++ book_title_wizzard.py
@@ -17,30 +17,39 @@ example:
 from random import randint
 
 
-def title_creator():
+def title_creator(book_list=[]):
     opening_db = ["ravens", "mirrors", "ants", "races", "magical items"]
     preposition_db = ["of the", "and", "without", "within", "of"]
     closing_db = ["humans", "glyphs", "island", "peninsula", "mushrooms"]
 
-    first_part = opening_db[randint(0, 4)].capitalize()
-    preposition = preposition_db[randint(0, 4)]
-    second_part = closing_db[randint(0, 4)]
+    for i in range(0, 5):
+        first_part = opening_db[randint(0, 4)].capitalize()
+        preposition = preposition_db[randint(0, 4)]
+        second_part = closing_db[randint(0, 4)]
 
-    book_title = '"' + first_part + " " + preposition + " " + second_part + '"'
-    print(book_title)
+        book_title = '"' + first_part + " " + preposition + " " + second_part + '"'
+
+        book_list.insert(i, book_title)
+
+    return book_list
 
 
-def author_creator():
+def author_creator(author_list=[]):
     name_db = ["deidre", "eric", "corvin", "oberon", "benedict"]
     surname_db = ["rose", "smith", "trimmer", "white", "rodriguez"]
 
-    name_part = name_db[randint(0, 4)].capitalize()
-    surname_db = surname_db[randint(0, 4)].capitalize()
+    for i in range(0, 5):
+        name_part = name_db[randint(0, 4)].capitalize()
+        surname_part = surname_db[randint(0, 4)].capitalize()
 
-    author = ' - ' + name_part + " " + surname_db
-    print(author)
+        author = ' - ' + name_part + " " + surname_part
+
+        author_list.insert(i, author)
+
+    return author_list
 
 
 class BookTitleWizardCreator:
-    title_creator()
-    author_creator()
+
+    final_list = [x + y for x, y in zip(title_creator(), author_creator())]
+    print(final_list)
